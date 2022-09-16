83a4025087637aae6206a83173f1c8075bdd9b7b
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-09-16 15:00:25 -0400

    Add a condition to close scanner when user wins

--- src/Assignment2Application.java
+++ src/Assignment2Application.java
@@ -7,13 +7,18 @@ public class Assignment2Application {
 	public static void main(String[] args) {
 		int userInput = 0;
 		int example = generateRandom();
-			
+		
+		
+		
 		Scanner scanner = new Scanner(System.in);
+
+		
+	
 		for(int i = 0; i < 5; i++) {
 			
 			userInput = Integer.parseInt(scanner.nextLine());
 			
-			checkMyNumber(userInput, example);
+			checkMyNumber(userInput, example, isGameOver);
 			if(userInput < 1 || userInput > 100) {
 				i--;
 			}
@@ -22,35 +27,51 @@ public class Assignment2Application {
 				System.out.println("You lose! The Number to guess was " + example);
 			}
 			
+			// Close Scanner and break the loop in case user wins
+			if(userInput == example) {
+				scanner.close();
+				break;
+			}
+			
+			
 		}
 		
 		
 		
 		
-
-		scanner.close();
+			
+			scanner.close();
+		
 		
 		
 	}
 	
    
-	private static int checkMyNumber(int input, int example) {
+	private static int checkMyNumber(int input, int example, boolean isGameOver) {
 		
 //			
 		if(input < 1 || input > 100) {
 			System.out.println("Your Guess is not in between 0 and 100. Try again.");
-			
+			System.out.println(example);
+			return input;
 		}	
 //			
 		else if( input < example) {
 			System.out.println("Your number is: " + input + " Pick a higher number");
+			System.out.println(example);
 			return input;
 		} else if (input > example) {
 			System.out.println("Your number is: " + input + " Pick a lower number");
+			System.out.println(example);
 			
 		return input;
 		} else if(input == example) {
 			System.out.println("You WIN!");
+			isGameOver = true;
+			System.out.println(example);
+			return input;
+			
+			
 		}
 		return input;
 		
